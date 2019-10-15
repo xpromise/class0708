768f0e56a668fcef653e0d580859c87b62e27eae
xiongjian <webjsforyou@163.com>
2019-10-15 16:39:44 +0800

    cookie使用

---
+++ nodejs/day06/07.try catch.js
@@ -0,0 +1,23 @@
+
+(async () => {
+
+  try {
+    // 放可能出错代码
+    await new Promise((resolve, reject) => {
+      resolve(111)
+    });
+
+    await new Promise((resolve, reject) => {
+      resolve('错误具体原因')
+    });
+
+    console.log(333);
+
+  } catch (error) {
+    // 一旦try中代码错误，就会停止try中代码运行，直接来catch。 由catch处理异常
+    console.log('--------');
+    console.log(error);
+    console.log('--------');
+  }
+
+})();
