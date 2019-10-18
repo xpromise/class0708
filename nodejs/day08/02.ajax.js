250065aabe5fab4875363ec537b7ffbc11546d3c
xiongjian <webjsforyou@163.com>
2019-10-18 11:13:25 +0800

    jquery的ajax的基本使用

--- nodejs/day08/02.ajax.js
+++ nodejs/day08/02.ajax.js
@@ -2,11 +2,21 @@ const express = require('express');
 const app = express();
 
 app.use(express.static('public'));
+// 解析请求体参数：只能解析采用urlencoded编码
+app.use(express.urlencoded({extended: true}));
+// 解析请求体参数：只能解析采用json编码
+app.use(express.json());
 
 app.get('/ajax', (req, res) => {
   console.log(req.query);
 
-  res.json({name: 'rose', age: 20});
+  res.json({name: 'tom', age: 55});
+});
+
+app.post('/ajax', (req, res) => {
+  console.log(req.body);
+
+  res.json({name: 'tom', age: 20});
 });
 
 app.listen(3000, (err) => {
