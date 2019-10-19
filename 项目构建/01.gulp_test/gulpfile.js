e77864cf85a275bbf7d5115ec656fa64e7077957
xiongjian <webjsforyou@163.com>
2019-10-19 10:47:35 +0800

    gulp-browserify

--- 项目构建/01.gulp_test/gulpfile.js
+++ 项目构建/01.gulp_test/gulpfile.js
@@ -16,6 +16,8 @@
 
 const gulp = require('gulp');
 const babel = require('gulp-babel');
+const browserify = require('gulp-browserify');
+const rename = require('gulp-rename');
 
 // 配置插件任务
 gulp.task('babel', () => {
@@ -24,3 +26,14 @@ gulp.task('babel', () => {
     .pipe(babel({presets: ['@babel/preset-env']})) // 使用babel插件将导入的文件进行语法装换（将ES6模块化语法装换成commonjs模块化语法, 将ES6其他语法装换成ES5一下的低级语法）
     .pipe(gulp.dest('build/js')) // 将流中文件输出/写入到另外文件夹里面去
 });
+
+gulp.task('browserify', function() {
+  return gulp.src('build/js/index.js')
+    .pipe(browserify()) // 将commonjs模块化装换浏览器能识别的js语法
+    .pipe(rename('built.js')) // 重命名
+    .pipe(gulp.dest('./build/js'))
+});
+
+// 配置默认任务， 实际执行的任务是babel和browserify
+gulp.task('default', gulp.series(['babel', 'browserify'])); // 同步：顺序执行
+// gulp.task('default', gulp.parallel(['babel', 'browserify'])); // 异步：并行执行
